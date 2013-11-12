8131eb219f2473a9658b5a9764a40e7ddda78eb6
wangqs1990 <283648304@qq.com>
2013-11-12 12:43:26 +0800

    fire callback list

--- promise.js
+++ promise.js
@@ -2,14 +2,17 @@
     "use strict"
      function Deferred() {
         this.state = "resolving";
-        this._callback = {};
+        this._callback = {
+            "resolved": [],
+            "rejected": [],
+            "process": []
+        };
     }
 
     var slice = [].slice;
 
     Deferred.prototype.resolve = function() {
         var res, 
-            callback = this._callback["done"],
             args = slice.call(arguments);
         this.result = args;
         if (this.state !== "resolving") {
@@ -17,44 +20,58 @@
         }
         try {
             this.state = "resolved";
-            callback && this._then(callback.apply(this, args));
+            this._fire()
+                ._clear();
         } catch(e) {
-            this._then(e, true);
+            this.state = "rejected";
+            this.result = e;
+            this._fire()
+                ._clear();
         }
-        this._callback = {};
+
         return this;
     };
 
-    Deferred.prototype._then = function(value, rejected) {
-        var then = this._next;
-        if(then) {
-            if(!rejected) {
-                then.resolve(value);
-            } else {
-                then.reject(value);
+    Deferred.prototype._fire = function() {
+        var state = this.state,
+            callbacks = this._callback[state],
+            args = this.result,
+            fn;
+        if (state === "resolving") {
+            for (var i = 0, length = callbacks.length; i < length; i++) {
+                args = callbacks.pop().call(this, args) || args;   
+            }
+        } else if (state === "rejected") {
+            for (var i = 0, length = callbacks.length; i < length; i++) {
+                callbacks.pop().call(this, args);   
             }
         }
         return this;
-    };
+    }
 
     Deferred.prototype._clear = function() {
-        this._callback = {};
+        this._callback = {
+            "resolved": [],
+            "rejected": [],
+            "process": []
+        };
         this.result = undefined;
+        return this;
     };
 
     Deferred.prototype.reject = function(reason) {
         this.state = "rejected";
-        var callback = this._callback["fail"];
-        callback && callback(reason);
-        this._then(reason, true)._clear();
+        this.result = reason;
+        this._fire()
+            ._clear();
+        return this;
     };
 
     Deferred.prototype.then = function(done, fail, process) {
-        this._callback["done"] = done;
-        this._callback["fail"] = fail;
-        this._callback["process"] = process;
-        this._next = new Deferred();
-        return this._next.promise();
+        done && this._callback["resolved"].push(done);
+        fail && this._callback["rejected"].push(fail);
+        process && this._callback["process"].push(process);
+        return this;
     };
 
     Deferred.prototype.promise = function() {
