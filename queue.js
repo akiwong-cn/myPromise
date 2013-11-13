8b2908aef7d9ffe7d413e3dd57e84c10836c27f3
wangqs1990 <283648304@qq.com>
2013-11-13 19:45:14 +0800

    last function is no need to call next

--- queue.js
+++ queue.js
@@ -7,13 +7,25 @@
 
     Queue.prototype._next = function () {
         var next = this._callback.shift();
-        if (!next) {
+        if (!next ) {
             return (function () { this.runned = true; this.running = false }).bind(this);
         }
-        return (function () {
-            next[1].push(this._next());
-            next[0].apply(this, next[1]);
-        }).bind(this);
+        return (function (length) {
+            return (function () {
+                var args = next[1];
+                //使得前一个方法可以注入参数到后一个方法
+                for (var i = 0, length = arguments.length; i < length; i++) {
+                    args.push(arguments[i]);
+                }
+                args.push(this._next());
+                next[0].apply(this, args);
+                //防止最后一步不调用next函数
+                if (!length) {
+                    this.runned = true;
+                    this.running = false;
+                }
+            }).bind(this);
+        }).call(this, this._callback.length);
     }
 
     Queue.prototype.push = function (fn) {
